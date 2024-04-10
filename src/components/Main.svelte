2288527ec3060cf649306649e94011509294d4d2
Sudhamshu Suri <se21uecm061@mahindrauniversity.edu.in>
2024-04-10 15:13:45 +0530

    centers the intro page text

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,3 @@
-suri
 <script>
     
 </script>
@@ -6,7 +5,7 @@ suri
 <main>
     <section id="introPage" cass="grid grid-cols-1 lg:grid-cols-2 gap 10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-6 md:gap-8 lg:gap-10">
-            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+            <h2 class=" text-center font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi, I am <span class="Reddit-Mono text-violet-400">Sudhamshu</span> Suri <br/>Intern @<span class="Reddit-Mono text-violet-400">Blank Labs</span>
             </h2>
         </div>
