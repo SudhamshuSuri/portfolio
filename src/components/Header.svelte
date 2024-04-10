51c0c93ec92ec495b4ea5b4f6d198b749c5daa45
Sudhamshu Suri <se21uecm061@mahindrauniversity.edu.in>
2024-04-10 17:05:04 +0530

    added link to home page in the name header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,9 +10,11 @@
 <header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid '+ (
     y > 0 ? 'pt-4 bg-slate-950 border-violet-950': 'py-6 bg-transparent border-transparent'
 )}>
+    <a href="#" target="_blank">
     <h1 class="font-medium">
         <b class="font-bold Reddit-Mono">Sudhamshu</b> Suri
     </h1>
+    </a>
     <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
         <a href={tab.link} class="duration-200 hover:text-violet-400">
@@ -20,7 +22,7 @@
         </a>
         {/each}
     </div>
-    <a href="#" target="_blank" class="blueShadow relative float overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <a href="https://github.com/SudhamshuSuri" target="_blank" class="blueShadow relative float overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
         <div class="absolute top-0 right-full w-full h-full bg-violet opacity-20 group-hover:translatex-full z-0 duration 200" />
         <h4 class="relative z-9">Get in Touch!</h4>
     </a>
