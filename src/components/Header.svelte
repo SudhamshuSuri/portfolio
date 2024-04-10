a36e1f0ca50ac340b55bec13cc6ddbbf411fbe50
Sudhamshu Suri <se21uecm061@mahindrauniversity.edu.in>
2024-04-10 15:10:43 +0530

    Added the Hi I am text

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -11,7 +11,7 @@
     y > 0 ? 'pt-4 bg-slate-950 border-violet-950': 'py-6 bg-transparent border-transparent'
 )}>
     <h1 class="font-medium">
-        <b class="font-bold Reddit-Mono">Sudhamshu</b>Suri
+        <b class="font-bold Reddit-Mono">Sudhamshu</b> Suri
     </h1>
     <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,14 @@
 suri
+<script>
+    
+</script>
+
+<main>
+    <section id="introPage" cass="grid grid-cols-1 lg:grid-cols-2 gap 10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi, I am <span class="Reddit-Mono text-violet-400">Sudhamshu</span> Suri <br/>Intern @<span class="Reddit-Mono text-violet-400">Blank Labs</span>
+            </h2>
+        </div>
+    </section>
+</main>
